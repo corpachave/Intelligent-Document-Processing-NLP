9a5fff192597a81a74796a7d407c2b4ffd8bb8b6
Corpachave <corpachave@gmail.com>
2026-02-26 19:40:06 +0530

    code to detect pdf type

---
+++ test_codes.py
@@ -0,0 +1,24 @@
+import pdfplumber
+
+# Detect PDF Type
+def is_scanned_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        first_page = pdf.pages[0]
+        text = first_page.extract_text()
+        return text is None
+
+# Digital PDF Extraction
+def extract_digital_text(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text = text + page_text + "\n"
+    return text
+
+pdf_path = r"E:\Learning\Zaamila Development\Projects\Fintech-Intelligent-Document-Processing-NLP\data\raw_pdfs\example-scan.pdf"
+if is_scanned_pdf(pdf_path):
+    print("This is a scanned pdf (Image based).")
+else:
+    print("This is a digital pdf (Text based).")
