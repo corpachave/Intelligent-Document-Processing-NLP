2220435406d235ad8aac4a8f33182a8ca2c63ddd
Corpachave <corpachave@gmail.com>
2026-02-26 19:50:46 +0530

    code for Digital pdf extraction

--- test_codes.py
+++ test_codes.py
@@ -17,8 +17,12 @@ def extract_digital_text(pdf_path):
                 text = text + page_text + "\n"
     return text
 
-pdf_path = r"E:\Learning\Zaamila Development\Projects\Fintech-Intelligent-Document-Processing-NLP\data\raw_pdfs\example-scan.pdf"
+pdf_path = r"E:\Learning\Zaamila Development\Projects\Fintech-Intelligent-Document-Processing-NLP\data\raw_pdfs\loan_agreement_01.pdf"
+
 if is_scanned_pdf(pdf_path):
     print("This is a scanned pdf (Image based).")
 else:
     print("This is a digital pdf (Text based).")
+
+extracted_text = extract_digital_text(pdf_path)
+print(extracted_text)
